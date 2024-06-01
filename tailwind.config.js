8ed75964e0f7c417fcfa89e74727f1b8a281dcbc
Vadim Belinovich <vadimbelinovich@gmail.com>
2024-06-01 21:17:53 +0300

    add config

---
+++ tailwind.config.js
@@ -0,0 +1,111 @@
+import animate from 'tailwindcss-animate';
+
+/** @type {string[]} */
+export const darkMode = ['class'];
+export const safelist = ['dark'];
+export const prefix = '';
+export const theme = {
+  container: {
+    center: true,
+    padding: {
+      DEFAULT: '1rem',
+      xl: '170px',
+    },
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1800px',
+    },
+  },
+  extend: {
+    colors: {
+      border: 'hsl(var(--border))',
+      input: 'hsl(var(--input))',
+      ring: 'hsl(var(--ring))',
+      background: 'hsl(var(--background))',
+      foreground: 'hsl(var(--foreground))',
+      primary: {
+        DEFAULT: 'hsl(var(--primary))',
+        foreground: 'hsl(var(--primary-foreground))',
+      },
+      secondary: {
+        DEFAULT: 'hsl(var(--secondary))',
+        foreground: 'hsl(var(--secondary-foreground))',
+      },
+      destructive: {
+        DEFAULT: 'hsl(var(--destructive))',
+        foreground: 'hsl(var(--destructive-foreground))',
+      },
+      success: {
+        DEFAULT: 'hsl(var(--success))',
+        foreground: 'hsl(var(--success-foreground))',
+      },
+      muted: {
+        DEFAULT: 'hsl(var(--muted))',
+        foreground: 'hsl(var(--muted-foreground))',
+      },
+      accent: {
+        DEFAULT: 'hsl(var(--accent))',
+        foreground: 'hsl(var(--accent-foreground))',
+      },
+      popover: {
+        DEFAULT: 'hsl(var(--popover))',
+        foreground: 'hsl(var(--popover-foreground))',
+      },
+      card: {
+        DEFAULT: 'hsl(var(--card))',
+        foreground: 'hsl(var(--card-foreground))',
+      },
+    },
+    borderRadius: {
+      xl: 'calc(var(--radius) + 4px)',
+      lg: 'var(--radius)',
+      md: 'calc(var(--radius) - 2px)',
+      sm: 'calc(var(--radius) - 4px)',
+    },
+    keyframes: {
+      'accordion-down': {
+        from: { height: 0 },
+        to: { height: 'var(--radix-accordion-content-height)' },
+      },
+      'accordion-up': {
+        from: { height: 'var(--radix-accordion-content-height)' },
+        to: { height: 0 },
+      },
+      'collapsible-down': {
+        from: { height: 0 },
+        to: { height: 'var(--radix-collapsible-content-height)' },
+      },
+      'collapsible-up': {
+        from: { height: 'var(--radix-collapsible-content-height)' },
+        to: { height: 0 },
+      },
+    },
+    animation: {
+      'accordion-down': 'accordion-down 0.2s ease-out',
+      'accordion-up': 'accordion-up 0.2s ease-out',
+      'collapsible-down': 'collapsible-down 0.2s ease-in-out',
+      'collapsible-up': 'collapsible-up 0.2s ease-in-out',
+    },
+    backgroundImage: {
+      'separator-pattern': 'url(\'../img/separator-circle.svg\')',
+    },
+    transitionProperty: {
+      'border': 'border-color',
+    },
+    screens: {
+      '2xl': '1800px',
+    },
+  },
+  fontFamily: {
+    'sans': 'Montserrat, sans-serif',
+    'display': 'Comfortaa, sans-serif',
+  },
+  colors: {
+    'notice': '#EDF6FC',
+    'notice-purple': '#ECE6F0',
+  },
+};
+export const plugins = [animate];
