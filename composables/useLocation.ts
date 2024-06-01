887276294a57b811c46cae9eb093f248e32a0293
Vadim Belinovich <vadimbelinovich@gmail.com>
2024-06-01 22:00:28 +0300

    add composables

---
+++ composables/useLocation.ts
@@ -0,0 +1,29 @@
+import {computed, type ComputedRef} from "vue";
+import {useRoute} from "vue-router";
+import type {Location} from "~/types";
+import branches from "~/data/branches";
+import { FLAGS } from '~/data/constants';
+
+export function useLocation(): ComputedRef<Location> {
+	const route = useRoute()
+	return computed(() => {
+		const path = route.path.split('/')[1];
+		if (path === '' || path === 'ru-ru') {
+			return {
+				currentLocation: "ru-ru",
+				newLocationPath: "by-ru",
+				newLocationText: "Рос",
+				branch: branches.spb,
+				flag: FLAGS.ru.image
+			}
+		} else {
+			return {
+				currentLocation: "by-ru",
+				newLocationPath: "ru-ru",
+				newLocationText: "Бел",
+				branch: branches.minsk,
+				flag: FLAGS.by.image
+			}
+		}
+	})
+}
