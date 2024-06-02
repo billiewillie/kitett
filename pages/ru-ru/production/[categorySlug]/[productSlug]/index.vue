d7962f97daf8beb28e992d12f170ab877810df9c
Vadim Belinovich <vadimbelinovich@gmail.com>
2024-06-03 00:00:53 +0300

    add company2

--- pages/ru-ru/production/[categorySlug]/[productSlug]/index.vue
+++ pages/ru-ru/production/[categorySlug]/[productSlug]/index.vue
@@ -1,64 +1,64 @@
-<!--<script-->
-<!--  setup-->
-<!--  lang="ts">-->
-<!--import { computed } from 'vue';-->
-<!--import { useRoute } from 'vue-router';-->
-<!--import ProductPageLayout from '~/layouts/ProductPageLayout.vue';-->
-<!--import { clearError } from '#app';-->
-<!--import products from '~/data/products';-->
-<!--import { useHead, useSeoMeta } from 'unhead';-->
+<script
+  setup
+  lang="ts">
+import {computed} from 'vue';
+import {useRoute} from 'vue-router';
+import ProductPageLayout from '~/layouts/ProductPageLayout.vue';
+import {clearError} from '#app';
+import products from '~/data/products';
+import {useHead, useSeoMeta} from 'unhead';
 
-<!--const route = useRoute();-->
+const route = useRoute();
 
-<!--const category = computed(() => {-->
-<!--  return products.categories.find((category) => category.slug === route.params.categorySlug);-->
-<!--});-->
+const category = computed(() => {
+  return products.categories.find((category) => category.slug === route.params.categorySlug);
+});
 
-<!--if (!category.value) {-->
-<!--  clearError({-->
-<!--    redirect: '/404',-->
-<!--  });-->
-<!--}-->
+if (!category.value) {
+  clearError({
+    redirect: '/404',
+  });
+}
 
-<!--const product = computed(() => {-->
-<!--  return category.value?.productList.find((product) => product.slug === route.params.productSlug);-->
-<!--});-->
+const product = computed(() => {
+  return category.value?.productList.find((product) => product.slug === route.params.productSlug);
+});
 
-<!--if (!product.value) {-->
-<!--  clearError({-->
-<!--    redirect: '/404',-->
-<!--  });-->
-<!--}-->
+if (!product.value) {
+  clearError({
+    redirect: '/404',
+  });
+}
 
-<!--useSeoMeta({-->
-<!--  ogTitle: `${product.value?.fullTitle} Kitett`,-->
-<!--  ogDescription: `${product.value?.description} Kitett`,-->
-<!--  ogImage: '../../../img/ogimage.jpg',-->
-<!--  ogUrl: 'https://kitett.com/',-->
-<!--  twitterTitle: `${product.value?.fullTitle} Kitett`,-->
-<!--  twitterDescription: `${product.value?.description} Kitett`,-->
-<!--  twitterImage: '../../../img/ogimage.jpg',-->
-<!--  twitterCard: 'summary',-->
-<!--});-->
+useSeoMeta({
+  ogTitle: `${product.value?.fullTitle} Kitett`,
+  ogDescription: `${product.value?.description} Kitett`,
+  ogImage: '../../../img/ogimage.jpg',
+  ogUrl: 'https://kitett.com/',
+  twitterTitle: `${product.value?.fullTitle} Kitett`,
+  twitterDescription: `${product.value?.description} Kitett`,
+  twitterImage: '../../../img/ogimage.jpg',
+  twitterCard: 'summary',
+});
 
-<!--useHead({-->
-<!--  title: `${product.value?.fullTitle}`,-->
-<!--  meta: [-->
-<!--    {-->
-<!--      name: 'description',-->
-<!--      content: `${product.value?.description}`,-->
-<!--    },-->
-<!--    {-->
-<!--      name: 'keywords',-->
-<!--      content: `${product.value?.description}`,-->
-<!--    },-->
-<!--  ],-->
-<!--});-->
-<!--</script>-->
+useHead({
+  title: `${product.value?.fullTitle}`,
+  meta: [
+    {
+      name: 'description',
+      content: `${product.value?.description}`,
+    },
+    {
+      name: 'keywords',
+      content: `${product.value?.description}`,
+    },
+  ],
+});
+</script>
 
 <template>
   <div></div>
-<!--  <ProductPageLayout-->
-<!--    :category="category"-->
-<!--    :product="product" />-->
+  <ProductPageLayout
+    :category="category"
+    :product="product" />
 </template>
