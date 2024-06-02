1c9a1721ea91133f3dae996d38adcd4ac9735c6c
Vadim Belinovich <vadimbelinovich@gmail.com>
2024-06-03 00:25:35 +0300

    add company

--- layouts/CategoryPageLayout.vue
+++ layouts/CategoryPageLayout.vue
@@ -1,72 +1,72 @@
-<!--<script-->
-<!--  setup-->
-<!--  lang="ts">-->
-<!--import { useLocation } from '~/composables/useLocation';-->
+<script
+  setup
+  lang="ts">
+import {useLocation} from '~/composables/useLocation';
 
-<!--const location = useLocation();-->
-<!--import type { Breadcrumb } from '~/types';-->
+const location = useLocation();
+import type {Breadcrumb} from '~/types';
 
-<!--const props = defineProps({-->
-<!--  products: {-->
-<!--    type: Array,-->
-<!--  },-->
-<!--  category: {-->
-<!--    type: Object,-->
-<!--  },-->
-<!--});-->
+const props = defineProps({
+  products: {
+    type: Array,
+  },
+  category: {
+    type: Object,
+  },
+});
 
-<!--const breadcrumbs: Breadcrumb[] = [-->
-<!--  {-->
-<!--    id: 1,-->
-<!--    title: 'главная',-->
-<!--    link: `/${location.value.currentLocation}`,-->
-<!--  },-->
-<!--  {-->
-<!--    id: 2,-->
-<!--    title: 'продукция',-->
-<!--    link: `/${location.value.currentLocation}/production`,-->
-<!--  },-->
-<!--  {-->
-<!--    id: 3,-->
-<!--    title: props.category.title,-->
-<!--  },-->
-<!--];-->
-<!--</script>-->
+const breadcrumbs: Breadcrumb[] = [
+  {
+    id: 1,
+    title: 'главная',
+    link: `/${location.value.currentLocation}`,
+  },
+  {
+    id: 2,
+    title: 'продукция',
+    link: `/${location.value.currentLocation}/production`,
+  },
+  {
+    id: 3,
+    title: props.category.title,
+  },
+];
+</script>
 
 <template>
   <section class="first-screen mb-[75px]">
-<!--    <div class="container m-auto">-->
-<!--      <BaseBreadcrumb :links="breadcrumbs" />-->
-<!--      <BaseTitle-->
-<!--        :title="props.category?.title"-->
-<!--        tag="h1"-->
-<!--        separator-style="top-[calc(50%-2px)] absolute"-->
-<!--        text-style="text-center uppercase" />-->
-<!--      <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 md:flex-row md:flex-wrap xl:flex-wrap-reverse w-full gap-8 xl:gap-[72px]">-->
-<!--        <div-->
-<!--          v-for="product in props.products"-->
-<!--          :key="product.id"-->
-<!--          class="flex flex-col items-center text-center w-full card">-->
+    <div class="container m-auto">
+      <BaseBreadcrumb :links="breadcrumbs"/>
+      <BaseTitle
+        :title="props.category?.title"
+        tag="h1"
+        separator-style="top-[calc(50%-2px)] absolute"
+        text-style="text-center uppercase"/>
+      <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 md:flex-row md:flex-wrap xl:flex-wrap-reverse w-full gap-8 xl:gap-[72px]">
+        <div
+          v-for="product in props.products"
+          :key="product.id"
+          class="flex flex-col items-center text-center w-full card">
 
-<!--          <BaseImage-->
-<!--            :image="product.cover"-->
-<!--            img-class="object-scale-down h-full w-full"-->
-<!--            class="flex justify-center items-center w-full md:h-[250px] xl:h-[300px] xl:px-4"-->
-<!--          />-->
+          <BaseImage
+            :image="product.cover"
+            img-class="object-scale-down h-full w-full"
+            class="flex justify-center items-center w-full md:h-[250px] xl:h-[300px] xl:px-4"
+          />
 
-<!--          <h3 class="text-2xl 2xl:text-3xl font-bold font-display md:min-h-[70px] xl:min-h-[70px] text-secondary mb-8">-->
-<!--            {{ product.title }}-->
-<!--          </h3>-->
+          <h3 class="text-2xl 2xl:text-3xl font-bold font-display md:min-h-[70px] xl:min-h-[70px] text-secondary mb-8">
+            {{ product.title }}
+          </h3>
 
-<!--          <Button as-child>-->
-<!--            <NuxtLink-->
-<!--              :to="`/ru-ru/production/${props.category.slug}/${product.slug}`"-->
-<!--              class="text-[18px] uppercase">-->
-<!--              открыть-->
-<!--            </NuxtLink>-->
-<!--          </Button>-->
-<!--        </div>-->
-<!--      </div>-->
-<!--    </div>-->
+          <Button as-child>
+            <NuxtLink
+              :to="`/ru-ru/production/${props.category.slug}/${product.slug}`"
+              class="text-[18px] uppercase">
+              открыть
+            </NuxtLink>
+          </Button>
+        </div>
+      </div>
+    </div>
   </section>
 </template>
--- layouts/ProductionPageLayout.vue
+++ layouts/ProductionPageLayout.vue
@@ -1,79 +1,79 @@
-<!--<script-->
-<!--  setup-->
-<!--  lang="ts">-->
-<!--import { useLocation } from '~/composables/useLocation';-->
-<!--import type { Breadcrumb } from '~/types';-->
-<!--import { ref } from 'vue';-->
+<script
+  setup
+  lang="ts">
+import {useLocation} from '~/composables/useLocation';
+import type {Breadcrumb} from '~/types';
+import {ref} from 'vue';
 
-<!--const location = useLocation();-->
+const location = useLocation();
 
-<!--const props = defineProps({-->
-<!--  products: {-->
-<!--    type: Object,-->
-<!--  },-->
-<!--});-->
+const props = defineProps({
+  products: {
+    type: Object,
+  },
+});
 
-<!--const breadcrumbs: Breadcrumb[] = [-->
-<!--  {-->
-<!--    id: 1,-->
-<!--    title: 'главная',-->
-<!--    link: `/${location.value.currentLocation}`,-->
-<!--  },-->
-<!--  {-->
-<!--    id: 2,-->
-<!--    title: 'продукция',-->
-<!--  },-->
-<!--];-->
+const breadcrumbs: Breadcrumb[] = [
+  {
+    id: 1,
+    title: 'главная',
+    link: `/${location.value.currentLocation}`,
+  },
+  {
+    id: 2,
+    title: 'продукция',
+  },
+];
 
-<!--const isImageLoaded = ref(false);-->
-<!--</script>-->
+const isImageLoaded = ref(false);
+</script>
 
 <template>
   <section class="first-screen mb-[72px] xl:mb-[100px]">
     <div class="container">
-<!--      <BaseBreadcrumb :links="breadcrumbs" />-->
+      <BaseBreadcrumb :links="breadcrumbs" />
 
-<!--      <BaseTitle-->
-<!--        title="продукция"-->
-<!--        tag="h1"-->
-<!--        separator-style="top-[calc(50%-2px)] absolute"-->
-<!--        text-style="text-center uppercase" />-->
+      <BaseTitle
+        title="продукция"
+        tag="h1"
+        separator-style="top-[calc(50%-2px)] absolute"
+        text-style="text-center uppercase" />
 
-<!--      <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 md:flex-row md:flex-wrap xl:flex-wrap-reverse w-full gap-8 xl:gap-[72px]">-->
-<!--        <div-->
-<!--          v-for="category in props.products?.categories"-->
-<!--          :key="category.id"-->
-<!--          class="group flex flex-col items-center text-center w-full card">-->
-<!--          <Icon-->
-<!--            name="cil:layers"-->
-<!--            class="absolute top-0 right-0 w-[32px] h-[32px] text-[#cfcfcf] group-hover:text-primary transition" />-->
-<!--          <div class="overflow-hidden">-->
-<!--            <NuxtPicture-->
-<!--              class="flex h-full w-full md:h-[250px] xl:h-[300px] xl:px-4 items-center justify-center transition-opacity"-->
-<!--              preload-->
-<!--              :img-attrs="{class: 'object-scale-down h-full w-full'}"-->
-<!--              :src="category.img" />-->
-<!--          </div>-->
+      <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 md:flex-row md:flex-wrap xl:flex-wrap-reverse w-full gap-8 xl:gap-[72px]">
+        <div
+          v-for="category in props.products?.categories"
+          :key="category.id"
+          class="group flex flex-col items-center text-center w-full card">
+          <Icon
+            name="cil:layers"
+            class="absolute top-0 right-0 w-[32px] h-[32px] text-[#cfcfcf] group-hover:text-primary transition"/>
+          <div class="overflow-hidden">
+            <NuxtPicture
+              class="flex h-full w-full md:h-[250px] xl:h-[300px] xl:px-4 items-center justify-center transition-opacity"
+              preload
+              :img-attrs="{class: 'object-scale-down h-full w-full'}"
+              :src="category.img"/>
+          </div>
 
-<!--          <h3 class="text-2xl 2xl:text-3xl font-bold font-display md:min-h-[70px] xl:min-h-[70px] text-secondary mb-8">-->
-<!--            {{ category.title }}-->
-<!--          </h3>-->
+          <h3 class="text-2xl 2xl:text-3xl font-bold font-display md:min-h-[70px] xl:min-h-[70px] text-secondary mb-8">
+            {{ category.title }}
+          </h3>
 
-<!--          <Separator class="my-[24px] w-1/2 xl:w-full h-[5px] bg-separator-pattern" />-->
+          <Separator class="my-[24px] w-1/2 xl:w-full h-[5px] bg-separator-pattern"/>
 
-<!--          <p class="md:min-h-[90px] xl:min-h-[90px] mb-[32px] text-base">-->
-<!--            {{ category.description }}-->
-<!--          </p>-->
+          <p class="md:min-h-[90px] xl:min-h-[90px] mb-[32px] text-base">
+            {{ category.description }}
+          </p>
 
-<!--          <Button as-child>-->
-<!--            <NuxtLink-->
-<!--              :to="`/${location.currentLocation}/production/${category.slug}`"-->
-<!--              class="text-[18px] uppercase">-->
-<!--              подробнее-->
-<!--            </NuxtLink>-->
-<!--          </Button>-->
-<!--        </div>-->
-<!--      </div>-->
+          <Button as-child>
+            <NuxtLink
+              :to="`/${location.currentLocation}/production/${category.slug}`"
+              class="text-[18px] uppercase">
+              подробнее
+            </NuxtLink>
+          </Button>
+        </div>
+      </div>
     </div>
   </section>
 </template>
