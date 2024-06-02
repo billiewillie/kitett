ac9624c68adb755d77682fae8f4073f34517260f
Vadim Belinovich <vadimbelinovich@gmail.com>
2024-06-02 23:17:44 +0300

    add company

--- layouts/ContactsPageLayout.vue
+++ layouts/ContactsPageLayout.vue
@@ -12,28 +12,6 @@ import {
 import {useLocation} from '~/composables/useLocation';
 import type {Branch, Breadcrumb} from '~/types';
 
-const map = shallowRef<null | YMap>(null);
-
-const coordinates = ref(branches.spb.map);
-
-const marker = ref({
-  coordinates: coordinates.value,
-});
-
-const location = useLocation();
-
-const breadcrumbs: Breadcrumb[] = [
-  {
-    id: 1,
-    title: 'главная',
-    link: `/${location.value.currentLocation}`,
-  },
-  {
-    id: 2,
-    title: 'контакты',
-  },
-];
-
 const branches: Record<string, Branch> = {
   spb: {
     id: 1,
@@ -87,6 +65,28 @@ const branches: Record<string, Branch> = {
   },
 };
 
+const map = shallowRef<null | YMap>(null);
+
+const coordinates = ref(branches.spb.map);
+
+const marker = ref({
+  coordinates: coordinates.value,
+});
+
+const location = useLocation();
+
+const breadcrumbs: Breadcrumb[] = [
+  {
+    id: 1,
+    title: 'главная',
+    link: `/${location.value.currentLocation}`,
+  },
+  {
+    id: 2,
+    title: 'контакты',
+  },
+];
+
 function setCoordinates(value) {
   coordinates.value = value;
 }
@@ -100,46 +100,46 @@ function scrollToMap() {
 </script>
 
 <template>
-<!--  <section class="first-screen mb-[75px]">-->
-<!--    <div class="container flex flex-col">-->
-<!--      <BaseBreadcrumb :links="breadcrumbs"/>-->
-<!--      <BaseTitle-->
-<!--        title="контакты"-->
-<!--        tag="h1"-->
-<!--        separator-style="top-[calc(50%-2px)] absolute"-->
-<!--        text-style="text-center uppercase"/>-->
-<!--      <div class="grid gap-[32px] grid-cols-1 xl:grid-cols-3 xl:min-h-[498px] xl:h-[1px]">-->
-<!--        <div class="xl:order-2">-->
-<!--          <h2 class="text-[30px] font-medium">Центральный офис:</h2>-->
-<!--          <br>-->
-<!--          <p>-->
-<!--            {{ branches.spb.index }}, {{ branches.spb.country }}, {{ branches.spb.city }},<br>{{ branches.spb.address }}-->
-<!--          </p>-->
-<!--          <br>-->
-<!--          <p>-->
-<!--            тел: -->
-<!--            <NuxtLink :href="`tel:${branches.spb.phone}`">-->
-<!--              {{ branches.spb.phone }}-->
-<!--            </NuxtLink>-->
-<!--          </p>-->
-<!--          <p>e-mail: -->
-<!--            <NuxtLink :href="`mailto:${branches.spb.email}`">-->
-<!--              {{ branches.spb.email }}-->
-<!--            </NuxtLink>-->
-<!--          </p>-->
-<!--          <br>-->
-<!--          <Button-->
-<!--            class="text-[18px]"-->
-<!--            @click="setCoordinates(branches.spb.map);scrollToMap()">посмотреть на карте-->
-<!--          </Button>-->
-<!--        </div>-->
-<!--        <BaseImage-->
-<!--          image="/img/contacts.jpg"-->
-<!--          img-class="object-cover w-full h-full"-->
-<!--          class="relative overflow-hidden top-0 w-full h-full bg-[#2363B6] col-span-2 rounded"/>-->
-<!--      </div>-->
-<!--    </div>-->
-<!--  </section>-->
+  <section class="first-screen mb-[75px]">
+    <div class="container flex flex-col">
+      <BaseBreadcrumb :links="breadcrumbs"/>
+      <BaseTitle
+        title="контакты"
+        tag="h1"
+        separator-style="top-[calc(50%-2px)] absolute"
+        text-style="text-center uppercase"/>
+      <div class="grid gap-[32px] grid-cols-1 xl:grid-cols-3 xl:min-h-[498px] xl:h-[1px]">
+        <div class="xl:order-2">
+          <h2 class="text-[30px] font-medium">Центральный офис:</h2>
+          <br>
+          <p>
+            {{ branches.spb.index }}, {{ branches.spb.country }}, {{ branches.spb.city }},<br>{{ branches.spb.address }}
+          </p>
+          <br>
+          <p>
+            тел: 
+            <NuxtLink :href="`tel:${branches.spb.phone}`">
+              {{ branches.spb.phone }}
+            </NuxtLink>
+          </p>
+          <p>e-mail: 
+            <NuxtLink :href="`mailto:${branches.spb.email}`">
+              {{ branches.spb.email }}
+            </NuxtLink>
+          </p>
+          <br>
+          <Button
+            class="text-[18px]"
+            @click="setCoordinates(branches.spb.map);scrollToMap()">посмотреть на карте
+          </Button>
+        </div>
+        <BaseImage
+          image="/img/contacts.jpg"
+          img-class="object-cover w-full h-full"
+          class="relative overflow-hidden top-0 w-full h-full bg-[#2363B6] col-span-2 rounded"/>
+      </div>
+    </div>
+  </section>
 
   <section
     id="map"
