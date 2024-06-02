97a62a468b3a1b4cdabb668954a8cedb37cdb376
Vadim Belinovich <vadimbelinovich@gmail.com>
2024-06-02 21:37:39 +0300

    add img

--- components/app/Header.vue
+++ components/app/Header.vue
@@ -1,114 +1,114 @@
-<!--<script-->
-<!--  setup-->
-<!--  lang="ts">-->
-<!--import { ref } from 'vue';-->
-<!--import BaseNavigation from '~/components/base/Navigation.vue';-->
-<!--import { useLocation } from '~/composables/useLocation';-->
-<!--import { Button } from '~/components/ui/button';-->
-<!--import {-->
-<!--  Dialog,-->
-<!--  DialogContent,-->
-<!--  DialogDescription,-->
-<!--  DialogHeader,-->
-<!--  DialogTitle, DialogTrigger,-->
-<!--} from '~/components/ui/dialog';-->
-<!--import { toast } from '~/components/ui/toast';-->
-<!--import { Input } from '~/components/ui/input';-->
-<!--import { useForm } from 'vee-validate';-->
-<!--import { toTypedSchema } from '@vee-validate/zod';-->
-<!--import * as z from 'zod';-->
-<!--import {-->
-<!--  FormControl,-->
-<!--  FormField,-->
-<!--  FormItem,-->
-<!--  FormLabel,-->
-<!--  FormMessage,-->
-<!--} from '@/components/ui/form';-->
-<!--import { useId, useRequestURL } from '#app';-->
-<!--import { SELECT_OPTIONS } from '~/data/constants';-->
-<!--import { VisuallyHidden } from 'radix-vue';-->
+<script
+  setup
+  lang="ts">
+import {ref} from 'vue';
+import BaseNavigation from '~/components/base/Navigation.vue';
+import {useLocation} from '~/composables/useLocation';
+import {Button} from '~/components/ui/button';
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle, DialogTrigger,
+} from '~/components/ui/dialog';
+import {toast} from '~/components/ui/toast';
+import {Input} from '~/components/ui/input';
+import {useForm} from 'vee-validate';
+import {toTypedSchema} from '@vee-validate/zod';
+import * as z from 'zod';
+import {
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import {useId, useRequestURL} from '#app';
+import {SELECT_OPTIONS} from '~/data/constants';
+import {VisuallyHidden} from 'radix-vue';
 
-<!--const location = useLocation();-->
+const location = useLocation();
 
-<!--const URL = useRequestURL();-->
+const URL = useRequestURL();
 
-<!--const nameId = useId();-->
-<!--const phoneId = useId();-->
-<!--const emailId = useId();-->
-<!--const topicId = useId();-->
-<!--const questionId = useId();-->
-<!--const checkId = useId();-->
+const nameId = useId();
+const phoneId = useId();
+const emailId = useId();
+const topicId = useId();
+const questionId = useId();
+const checkId = useId();
 
-<!--const phoneRegex = new RegExp(/^([+]?[\s0-9]+)?(\d{3}|[(]?[0-9]+[)])?(-?\s?[0-9])+$/);-->
+const phoneRegex = new RegExp(/^([+]?[\s0-9]+)?(\d{3}|[(]?[0-9]+[)])?(-?\s?[0-9])+$/);
 
-<!--const formSchema = toTypedSchema(z.object({-->
-<!--  name: z-->
-<!--    .string({ message: 'Введите ФИО' })-->
-<!--    .min(2, { message: 'Мало символов' })-->
-<!--    .max(50, { message: 'Много символов' }),-->
-<!--  phone: z-->
-<!--    .string({ message: 'Введите телефон' })-->
-<!--    .min(5, 'Неправильный номер телефона')-->
-<!--    .regex(phoneRegex, 'Неправильный номер телефона'),-->
-<!--  email: z-->
-<!--    .string({ message: 'Введите e-mail' })-->
-<!--    .email('Неправильный e-mail')-->
-<!--    .min(5, 'Мало символов'),-->
-<!--  topic: z.string({ message: 'Выберите тему' }),-->
-<!--  check: z.boolean({ message: 'Подтвердите согласие' }),-->
-<!--  question: z-->
-<!--    .string({ message: 'Напишите вопрос' })-->
-<!--    .min(5, { message: 'Мало символов' }),-->
-<!--}));-->
+const formSchema = toTypedSchema(z.object({
+  name: z
+    .string({message: 'Введите ФИО'})
+    .min(2, {message: 'Мало символов'})
+    .max(50, {message: 'Много символов'}),
+  phone: z
+    .string({message: 'Введите телефон'})
+    .min(5, 'Неправильный номер телефона')
+    .regex(phoneRegex, 'Неправильный номер телефона'),
+  email: z
+    .string({message: 'Введите e-mail'})
+    .email('Неправильный e-mail')
+    .min(5, 'Мало символов'),
+  topic: z.string({message: 'Выберите тему'}),
+  check: z.boolean({message: 'Подтвердите согласие'}),
+  question: z
+    .string({message: 'Напишите вопрос'})
+    .min(5, {message: 'Мало символов'}),
+}));
 
-<!--const form = useForm({-->
-<!--  validationSchema: formSchema,-->
-<!--});-->
+const form = useForm({
+  validationSchema: formSchema,
+});
 
-<!--const onSubmit = form.handleSubmit(async (values) => {-->
-<!--  try {-->
-<!--    const formData = {-->
-<!--      check: values.check,-->
-<!--      email: values.email,-->
-<!--      name: values.name,-->
-<!--      phone: values.phone,-->
-<!--      question: values.question,-->
-<!--      topic: values.topic,-->
-<!--    };-->
+const onSubmit = form.handleSubmit(async (values) => {
+  try {
+    const formData = {
+      check: values.check,
+      email: values.email,
+      name: values.name,
+      phone: values.phone,
+      question: values.question,
+      topic: values.topic,
+    };
 
-<!--    const data = await $fetch('https://new-event.online/api/feedback', {-->
-<!--      method: 'POST',-->
-<!--      body: formData,-->
-<!--    });-->
+    const data = await $fetch('https://new-event.online/api/feedback', {
+      method: 'POST',
+      body: formData,
+    });
 
-<!--    if (data === '200') {-->
-<!--      isOpen.value = false;-->
-<!--      form.resetForm();-->
-<!--      toast({-->
-<!--        description: 'Заявка отправлена!',-->
-<!--        variant: 'success',-->
-<!--      });-->
-<!--    }-->
-<!--  } catch (e) {-->
-<!--    toast({-->
-<!--      description: 'Заявка не отправлена!',-->
-<!--      variant: 'destructive',-->
-<!--    });-->
-<!--  }-->
-<!--});-->
+    if (data === '200') {
+      isOpen.value = false;
+      form.resetForm();
+      toast({
+        description: 'Заявка отправлена!',
+        variant: 'success',
+      });
+    }
+  } catch (e) {
+    toast({
+      description: 'Заявка не отправлена!',
+      variant: 'destructive',
+    });
+  }
+});
 
-<!--const isOpen = ref(false);-->
+const isOpen = ref(false);
 
-<!--const dialogToggle = () => {-->
-<!--  isOpen.value = !isOpen.value;-->
-<!--};-->
-<!--</script>-->
+const dialogToggle = () => {
+  isOpen.value = !isOpen.value;
+};
+</script>
 
 <template>
   <header class="shadow-lg py-[16px] xl:py-[32px]">
-<!--    <div class="relative container flex items-center justify-between">-->
-<!--      <BaseLogo :with-text="true" />-->
-<!--      <BaseNavigation />-->
+    <div class="relative container flex items-center justify-between">
+      <BaseLogo :with-text="true"/>
+      <BaseNavigation/>
 <!--      <Dialog :open="isOpen">-->
 <!--        <DialogTrigger as-child>-->
 <!--          <Button-->
@@ -142,9 +142,9 @@
 <!--                          type="text"-->
 <!--                          :id="nameId"-->
 <!--                          placeholder="ФИО"-->
-<!--                          v-bind="componentField" />-->
+<!--                          v-bind="componentField"/>-->
 <!--                      </FormControl>-->
-<!--                      <FormMessage />-->
+<!--                      <FormMessage/>-->
 <!--                    </FormItem>-->
 <!--                  </FormField>-->
 
@@ -157,9 +157,9 @@
 <!--                          type="tel"-->
 <!--                          :id="phoneId"-->
 <!--                          placeholder="Телефон"-->
-<!--                          v-bind="componentField" />-->
+<!--                          v-bind="componentField"/>-->
 <!--                      </FormControl>-->
-<!--                      <FormMessage />-->
+<!--                      <FormMessage/>-->
 <!--                    </FormItem>-->
 <!--                  </FormField>-->
 
@@ -172,9 +172,9 @@
 <!--                          type="email"-->
 <!--                          :id="emailId"-->
 <!--                          placeholder="E-mail"-->
-<!--                          v-bind="componentField" />-->
+<!--                          v-bind="componentField"/>-->
 <!--                      </FormControl>-->
-<!--                      <FormMessage />-->
+<!--                      <FormMessage/>-->
 <!--                    </FormItem>-->
 <!--                  </FormField>-->
 
@@ -187,7 +187,7 @@
 <!--                          <SelectTrigger :id="topicId">-->
 <!--                            <SelectValue-->
 <!--                              placeholder="Тема обращения"-->
-<!--                              class="text-secondary" />-->
+<!--                              class="text-secondary"/>-->
 <!--                          </SelectTrigger>-->
 <!--                        </FormControl>-->
 <!--                        <SelectContent>-->
@@ -201,7 +201,7 @@
 <!--                          </SelectGroup>-->
 <!--                        </SelectContent>-->
 <!--                      </Select>-->
-<!--                      <FormMessage />-->
+<!--                      <FormMessage/>-->
 <!--                    </FormItem>-->
 <!--                  </FormField>-->
 
@@ -220,7 +220,7 @@
 <!--                        v-bind="componentField"-->
 <!--                      />-->
 <!--                      </FormControl>-->
-<!--                      <FormMessage />-->
+<!--                      <FormMessage/>-->
 <!--                    </div>-->
 <!--                  </FormItem>-->
 <!--                </FormField>-->
@@ -238,14 +238,14 @@
 <!--                    <FormControl :id="checkId">-->
 <!--                      <Checkbox-->
 <!--                        :checked="value"-->
-<!--                        @update:checked="handleChange" />-->
+<!--                        @update:checked="handleChange"/>-->
 <!--                    </FormControl>-->
 <!--                    <div class="space-y-1 leading-none">-->
 <!--                      <FormLabel :forId="checkId">-->
 <!--                        Я согласен(на) на обработку персональных данных.-->
 <!--                        ООО "БиоЛайн" гарантирует конфиденциальность получаемой информации.-->
 <!--                      </FormLabel>-->
-<!--                      <FormMessage />-->
+<!--                      <FormMessage/>-->
 <!--                    </div>-->
 <!--                  </FormItem>-->
 <!--                </FormField>-->
@@ -254,21 +254,21 @@
 <!--          </div>-->
 <!--        </DialogContent>-->
 <!--      </Dialog>-->
-<!--      <Button-->
-<!--        as-child-->
-<!--        variant="outline">-->
-<!--        <NuxtLink-->
-<!--          class="hidden xl:flex absolute left-[calc(100%-135px)] items-end leading-none gap-1"-->
-<!--          :to="`/${location.newLocationPath}`">-->
-<!--          {{ location.newLocationText }} <img-->
-<!--          class="w-[20px] h-[16px] object-contain border rounded"-->
-<!--          :src="`${URL.origin}/${location.flag}`"-->
-<!--          :alt="location.newLocationPath"/>-->
-<!--        </NuxtLink>-->
-<!--      </Button>-->
-<!--      <div class="2xl:hidden">-->
-<!--        <AppMobileMenu />-->
-<!--      </div>-->
-<!--    </div>-->
+      <Button
+        as-child
+        variant="outline">
+        <NuxtLink
+          class="hidden xl:flex absolute left-[calc(100%-135px)] items-end leading-none gap-1"
+          :to="`/${location.newLocationPath}`">
+          {{ location.newLocationText }} <img
+          class="w-[20px] h-[16px] object-contain border rounded"
+          :src="`${URL.origin}/${location.flag}`"
+          :alt="location.newLocationPath"/>
+        </NuxtLink>
+      </Button>
+      <div class="2xl:hidden">
+        <AppMobileMenu/>
+      </div>
+    </div>
   </header>
 </template>
