668f0ef89d12e0e2ad7d46e62129acc0588ef69e
Vadim Belinovich <vadimbelinovich@gmail.com>
2024-06-02 20:59:08 +0300

    add config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,73 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
-  modules: ["@nuxt/image", "@nuxtjs/tailwindcss", "shadcn-nuxt"]
+	devtools: {enabled: true},
+	modules: [
+		'@nuxt/image',
+		'@nuxtjs/google-fonts',
+		'@nuxtjs/tailwindcss',
+		'nuxt-icon',
+		'shadcn-nuxt',
+		'@vueuse/nuxt',
+		'vue-yandex-maps/nuxt'
+	],
+	image: {
+		format: ['webp'],
+	},
+	tailwindcss: {
+		cssPath: ['~/assets/css/main.css', {injectPosition: 'first'}],
+		configPath: 'tailwind.config',
+		exposeConfig: {
+			level: 2,
+		},
+		config: {},
+		viewer: true,
+		editorSupport: true,
+	},
+	experimental: {
+		typedPages: true,
+	},
+	imports: {
+		autoImport: false,
+	},
+	shadcn: {
+		/**
+		 * Prefix for all the imported component
+		 */
+		prefix: '',
+		/**
+		 * Directory that the component lives in.
+		 * @default "./components/ui"
+		 */
+		componentDir: './components/ui',
+	},
+	yandexMaps: {
+		apikey: '3c059258-c2fe-4ad1-85b6-f124123aa8e1',
+	},
+	app: {
+		head: {
+			title: 'Товары для грудного вскармливания – Kitett',
+			meta: [
+				{
+					name: 'description',
+					content: 'Ручные и электрические молокоотсосы Kitett, а также советы в помощь кормящей маме',
+				},
+				{
+					name: 'keywords',
+					content: 'Ручные и электрические молокоотсосы Kitett, а также советы в помощь кормящей маме',
+				},
+				{
+					name: 'robots',
+					content: 'noindex',
+				},
+			],
+
+		},
+	},
+	typescript: {
+		tsConfig: {
+			compilerOptions: {
+				baseUrl: '.',
+			},
+		},
+	},
 })
